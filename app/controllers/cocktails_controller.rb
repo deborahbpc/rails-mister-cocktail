d1bc2865b32dd1bcf6ba8b8c190a45238968f82c
Deborah Carvalho <deborah.bpc@gmail.com>
2020-08-21 16:24:09 -0300

    some changes

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -35,7 +35,7 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :description)
+    params.require(:cocktail).permit(:name, :description, :photo)
   end
 
   def search_params
